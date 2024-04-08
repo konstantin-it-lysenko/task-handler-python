d32b831a8b6a79290dd1d3af5ab40737c84c5382
konstantin-it-lysenko <78411901+konstantin-it-lysenko@users.noreply.github.com>
2024-04-09 01:03:48 +0300

    add: user postgres model install: argon2

--- models.py
+++ models.py
@@ -1,6 +1,30 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
 from database import Base
 from datetime import datetime
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
+ph = PasswordHasher()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(String, primary_key=True, index=True)
+    created_at = Column(String, default=datetime.now())
+    updated_at = Column(Date)
+    email = Column(String, unique=True)
+    name = Column(String)
+    password = Column(String)
+
+    def set_password(self, password):
+        self.password = ph.hash(password)
+
+    def check_password(self, password):
+        try:
+            return ph.verify(self.password, password)
+        except VerifyMismatchError:
+            return False
 
 
 class Tasks(Base):
