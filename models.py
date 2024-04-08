c3d3d5373b9cc7216fe46764bb4efd844dd94d29
konstantin-it-lysenko <78411901+konstantin-it-lysenko@users.noreply.github.com>
2024-04-08 23:31:48 +0300

    add: time-block postgres model upd: task nullable fields

--- models.py
+++ models.py
@@ -3,15 +3,24 @@ from database import Base
 from datetime import datetime
 
 
-class Tasks(Base):
-    __tablename__ = 'tasks'
+class Task(Base):
+    __tablename__ = 'task'
 
     id = Column(String, primary_key=True, index=True)
     created_at = Column(Date, default=datetime.now())
     updated_at = Column(Date)
     name = Column(String, index=True)
-    priority = Column(String)
-    is_completed = Column(Boolean, default=False)
+    priority = Column(String, nullable=True)
+    is_completed = Column(Boolean, default=False, nullable=True)
 
 
+class TimeBlock(Base):
+    __tablename__ = 'time_block'
 
+    id = Column(String, primary_key=True, index=True)
+    created_at = Column(Date, default=datetime.now())
+    updated_at = Column(Date)
+    name = Column(String)
+    color = Column(String, nullable=True)
+    duration = Column(Integer)
+    order = Column(Integer)
